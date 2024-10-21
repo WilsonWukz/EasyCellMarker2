62354ac7aa491c0f709c88593fd1ac4813de79de
wilsonngfosiu <111402789+WilsonWukz@users.noreply.github.com>
2024-10-21 15:32:06 +0800

    Update SRC.R

--- R/SRC.R
+++ R/SRC.R
@@ -2,49 +2,44 @@ library(readxl)
 library(dplyr)
 library(ggplot2)
 
-
-get_marker <- function(spc, tsuClass, tsuType, cellname, file_path = NULL, plot = TRUE) {
-  library(readxl)
-  library(dplyr)
-  library(ggplot2)
-
+get_marker <- function(cellname, spc = NULL, tsuClass = NULL, tsuType = NULL, file_path = NULL, plot = TRUE, output_limit = 10) {
+  
   plot <- as.logical(plot)
-
-
+  
   if (is.null(file_path)) {
     file_path <- system.file("extdata", "Cell_marker_Human.xlsx", package = "EasyCellMarker2")
   }
-
-  # Read Excel
+  
   cell_marker_data <- read_excel(file_path)
-
-  # Filter
+  
   filtered_data <- cell_marker_data %>%
-    filter(species == !!spc,
-           tissue_class == !!tsuClass,
-           tissue_type == !!tsuType,
-           cell_name == !!cellname)
-
-  # Extract Columns ：cell_name, marker, PMID
+    filter(cell_name == !!cellname) %>%
+    {if (!is.null(spc)) filter(., species == !!spc) else .} %>%
+    {if (!is.null(tsuClass)) filter(., tissue_class == !!tsuClass) else .} %>%
+    {if (!is.null(tsuType)) filter(., tissue_type == !!tsuType) else .}
+  
   result <- filtered_data %>%
     select(cell_name, marker, PMID) %>%
-    mutate(marker = ifelse(is.na(marker) | marker == "", "NA", marker),    # 处理 marker 为空的情况
-           PMID = ifelse(is.na(PMID) | PMID == "", "NA", PMID))             # 处理 PMID 为空的情况
-
-  # Output
-  formatted_result <- paste0('"', result$marker, '", PMID: ', result$PMID, collapse = "; ")
-
-  # Print
+    mutate(marker = ifelse(is.na(marker) | marker == "", "NA", marker),
+           PMID = ifelse(is.na(PMID) | PMID == "", "NA", PMID))
+  
+  if (nrow(result) > output_limit) {
+    limited_result <- head(result, output_limit)
+    message("The displayed results exceed the upper limit. Only the first ", output_limit, " results.")
+  } else {
+    limited_result <- result
+  }
+  
+  formatted_result <- paste0('"', limited_result$marker, '", PMID: ', limited_result$PMID, collapse = "; ")
+  
   cat(cellname, ": ", formatted_result, "\n", sep = "")
-
-  # Count
+  
   marker_counts <- result %>%
     group_by(marker) %>%
     summarise(count = n()) %>%
     arrange(desc(count))
-
-  # Visualization
-  if (plot) {
+  
+  if (plot && nrow(marker_counts) > 0) {
     ggplot(marker_counts, aes(x = reorder(marker, -count), y = count)) +
       geom_bar(stat = "identity", fill = "steelblue") +
       coord_flip() +
@@ -54,9 +49,4 @@ get_marker <- function(spc, tsuClass, tsuType, cellname, file_path = NULL, plot 
       theme_minimal()
   }
 }
-
-
-# Samples
-get_marker(spc = "Human", tsuClass = "Abdomen", tsuType = "Abdomen", cellname = "Macrophage")
-get_marker(spc = "Human", tsuClass = "Breast", tsuType = "Breast", cellname = "B cell")
 
